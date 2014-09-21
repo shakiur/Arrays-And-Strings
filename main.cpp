a1baf5878d4ad7deedf8fcfaea58d086bae18e15
Shakiur <shakiur@gmail.com>
2014-09-21 10:57:13 -0400

    Add function remove all lets from string1 that appear in string2

--- main.cpp
+++ main.cpp
@@ -6,17 +6,18 @@ using namespace std;
 // Find first non-repeated character
 char findNonRepeatedChar(string);
 
+string removeCharFromString(string, string);
+
 int main() {
-    string test = "total";
+    string test1 = "total";
+    string test2 = "Battle of the Vowels: Hawaii vs. Grozny";
+    string rmv_test2 = "aeiou";
 
-    char first_char = findNonRepeatedChar(test);
+    char first_char = findNonRepeatedChar(test1);
 
-    cout << "First Single Char in: '" << test << "' is '";
+    string new_test = removeCharFromString(test2, rmv_test2);
 
-    if (first_char != '\0')
-        cout << first_char << "'\n";
-    else
-        cout << "NONE_FOUND" << "'\n";
+    cout << new_test << endl;
     
 }
 
@@ -31,7 +32,7 @@ char findNonRepeatedChar(string test) {
         letter_cnt_arr[i] = 0;
     }
 
-    // Store the count of each letter in a map
+    // Store the count of each letter in an array
     for(int i = 0; i < test_len; i++) {
         letter_cnt_arr[int(test[i])]++;
     }
@@ -43,5 +44,37 @@ char findNonRepeatedChar(string test) {
             return test[i];
     }
 
+    // Return if no non-repeated characters are found
     return '\0';
+}
+
+string removeCharFromString(string orig_str, string rmv_str) {
+    int orig_len = orig_str.length();
+    int rmv_len = rmv_str.length();
+
+    // New string to build new string
+    string new_str;
+
+    // Array to hold which characters to remove
+    int rmv_ascii_arr[128];
+
+    // Fill array with 0's so they can be incremented
+    for(int i = 0; i < 128; i++) {
+        rmv_ascii_arr[i] = 0;
+    }
+
+    // Flag characters that need to be removed
+    for(int i = 0; i < orig_len; i++) {
+        rmv_ascii_arr[int(rmv_str[i])]++;
+    }
+
+    // Go through original string and keep chars that aren't flagged
+    for(int i = 0; i < orig_len; i++) {
+        if(rmv_ascii_arr[int(orig_str[i])] == 0)
+            new_str += orig_str[i];
+    }
+
+    return new_str;
+
+
 }
